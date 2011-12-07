0d6f6e70747a5f4a8b11dc66401b5bc8e13b3e19
Sahil Yakhmi <sahil@dontemail.com>
2011-12-07 12:22:27 -0500

    added newarr to ast

--- ast.ml
+++ ast.ml
@@ -24,6 +24,7 @@ type expr =
 	| Litstring of string
 	| Litfunc of string list * func_expr
 	| Litarr of expr list
+	| Newarr of int list
 	| Id of string
 	| Binop of expr * bop * expr
 	| Unop of uop * expr
