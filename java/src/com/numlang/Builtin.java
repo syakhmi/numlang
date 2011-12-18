2ee751ec3bae8056fb59238b4fef6e2106358c16
dan <dan@dan-laptop.(none)>
2011-12-18 11:15:33 -0500

    Changes to builtin. Must checkin before removing

--- java/src/com/numlang/Builtin.java
+++ java/src/com/numlang/Builtin.java
@@ -1,19 +1,55 @@
 package com.numlang;
 
-public class Builtin {
-	public static void print(String str) {
+import java.util.Scanner;
+import java.util.LinkedList;
+import com.numlang.*;
+
+
+
+public class NumLang
+{
+	private static LinkedList<String> Stack = new LinkedList<String>();
+
+	public static NumLangIO		IO = new NumLangIO();		
+	public static NumLangBuiltin	Builtin = new Builtin()l
+	public static NumLangException 	Exception = new NumLangException();
+}
+
+class NumLangException
+{
+	public NumLangException(){};
+
+	public void DivideByZero(NumValue dividend, NumValue divisor)
+	{
+		System.err.println("NumLang Error: Divide By Zero");
+		System.exit(1);
+	}
+
+}
+
+class NumLangBuiltin {
+	public NumLangBuiltin(){}
+
+	public String str(NumValue num) {
+		return num.getValue().toString();
+	}
+
+	public NumValue num(String str) {
+		return new NumValue(new BigRational(str));
+	}
+}
+
+class NumLangIO
+{
+	private Scanner input;
+
+	public NumLangIo(){input = new Scanner(System.in)}
+	
+	public void print(String str) {
 		System.out.print(str);
 	}
 
-	public static void println(String str) {
+	public void println(String str) {
 		System.out.println(str);
-	}
-
-	public static String str(BigRational num) {
-		return num.toString();
-	}
-
-	public static BigRational num(String str) {
-		return new BigRational(str);
 	}
 }
